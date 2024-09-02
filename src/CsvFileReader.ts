1d2b1d9a9fd2ac1786908676c998df770a6080e8
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2024-09-02 10:07:56 -0400

    refactor: Update file reading logic to use fs module

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,8 +1,9 @@
 import fs from "fs";
+import { stringToDate } from "./utils";
 
 export class CsvFileReader {
   _data: string[][] = [];
-  constructor(public fileName: string) {}
+  constructor(public fileName: string) { }
 
   Read(): void {
     this._data = fs
@@ -12,7 +13,16 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      });
+      })
+      .map((row: string[]): any => {
+        return [
+          stringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+        ]
+      })
   }
 
   get data(): string[][] {
