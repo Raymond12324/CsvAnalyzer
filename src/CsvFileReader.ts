63c7e264086e1bc68877a4d7fc4a61f4bb5a1915
Randolph Raymond Carmona <raymondcarmonatejeda01@gmail.com>
2024-09-03 10:00:21 -0400

    refactor: Update file reading logic to use fs module and add MatchResult enum and a tuple

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
 import { stringToDate } from "./utils";
+import { MatchResult } from "./MatchResult";
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  _data: string[][] = [];
-  constructor(public fileName: string) { }
-
+  _data: MatchData[] = [];
+  constructor(public fileName: string) {}
   Read(): void {
     this._data = fs
       .readFileSync(`Data/${this.fileName}`, {
@@ -14,18 +16,20 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map((row: string[]): any => {
+      .map((row: string[]): MatchData => {
         return [
           stringToDate(row[0]),
           row[1],
           row[2],
           parseInt(row[3]),
           parseInt(row[4]),
-        ]
-      })
+          row[5] as MatchResult,
+          row[6],
+        ];
+      });
   }
 
-  get data(): string[][] {
+  get data(): MatchData[] {
     return this._data;
   }
 }
