79e797bf4e901702a0287fc10ace3864337cbf52
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2024-09-06 08:25:11 -0400

    refactor: Update file reading logic to use fs module and add MatchResult enum and a tuple

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
 
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
 const matchReader = MatchReader.fromCsv("Data/football.csv");
 const summary = Summary.winsAnalisisWithHtmlReport("Arsenal", "ArsenalWins");
 
